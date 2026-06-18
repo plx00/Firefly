81413aa60cfed02aa400567c40ed9e88c01b8a7d
plx00 <shook123@qq.com>
2026-06-18 17:47:34 +0800

    .

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -61,6 +61,15 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true,
 	},
 	{
+		title: "Hyde Blog",
+		imgurl: "https://seasir.top/assets/avatar.avif",
+		desc: "人心中的成见是一座大山",
+		siteurl: "https://seasir.top/",
+		tags: ["Blog"],
+		weight: 9,
+		enabled: true,
+	},
+	{
 		title: "Firefly Docs",
 		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
 		desc: "Firefly主题模板文档",
